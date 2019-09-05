bee1d95ce3f5af5e6ee0fc0497ba91075f113ea8
Farah Davoodi <farahdavoodi@gmail.com>
2019-09-04 22:18:37 -0700

    Added additional test for find_by_date method in Booker

--- test/booker_test.rb
+++ test/booker_test.rb
@@ -25,4 +25,15 @@ describe 'find_by_date' do
 
     expect( reserving_room.find_by_date("3/9/2019") ).must_be_kind_of Array
   end
+
+  it "should return nil if no reservations if no date matched" do
+    start_date = Time.parse("3/9/2019")
+    end_date = Time.parse("5/9/2019")
+
+    reserving_room = Hotel::Booker.new
+
+    reserving_room.reserve_room(start_date,end_date)
+    
+    expect( reserving_room.find_by_date("1/9/2019") ).must_be_empty
+  end
 end
